232308f40d71f7217f162717cbacb8f2f14a2058
Simon Braitsch <simon.braitsch@novatec-gmbh.de>
2022-06-01 11:15:52 +0200

    update assignment text

--- src/com/itag/oop/Main.java
+++ src/com/itag/oop/Main.java
@@ -3,10 +3,10 @@ package com.itag.oop;
 public class Main {
 
     public static void main(String[] args) {
-        Introduction intro = new Introduction();
+        Introduction intro = new Introduction(State.OK);
         intro.increment();
         System.out.println("--------------------");
-        Introduction intro2 = new Introduction();
+        Introduction intro2 = new Introduction(State.OK);
         intro2.increment();
     }
 }
